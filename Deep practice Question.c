61b787539abf26fe780b951adbc3feebc10e4397
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-07 18:36:09 +0530

    xyz

---
+++ Deep practice Question.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a[5],r,c,t;
+    char n[5][30],tm[30];
+    printf("enter 5 name and per% :- \n");
+    for(r=0; r<5; r++)
+    {
+        scanf("%[^\n]s",n[r]);
+        
+                strcpy(tm,n[c]);
+                strcpy(n[c],n[c+1]);
+                strcpy(n[c+1],tm);
+            }
+            
+    %s\t%d\n",n[r],a[r]);
+        
+    }
+    
+}
